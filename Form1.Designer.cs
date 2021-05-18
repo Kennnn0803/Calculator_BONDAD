9d23a6554dfc6de9587f5f34daf58abc5a822595
dischargetherest <aaron.kenn.bondad@gmail.com>
2021-05-18 12:06:49 +0800

    third try

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,6 +35,20 @@ namespace Calculator_BONDAD
             this.bfour = new System.Windows.Forms.Button();
             this.bone = new System.Windows.Forms.Button();
             this.bzero = new System.Windows.Forms.Button();
+            this.beight = new System.Windows.Forms.Button();
+            this.bnine = new System.Windows.Forms.Button();
+            this.bfive = new System.Windows.Forms.Button();
+            this.bsix = new System.Windows.Forms.Button();
+            this.btwo = new System.Windows.Forms.Button();
+            this.bthree = new System.Windows.Forms.Button();
+            this.bdot = new System.Windows.Forms.Button();
+            this.baddition = new System.Windows.Forms.Button();
+            this.bsubtraction = new System.Windows.Forms.Button();
+            this.bequal = new System.Windows.Forms.Button();
+            this.bmultiplication = new System.Windows.Forms.Button();
+            this.bdivision = new System.Windows.Forms.Button();
+            this.bclearentry = new System.Windows.Forms.Button();
+            this.bclear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // bseven
@@ -78,14 +92,154 @@ namespace Calculator_BONDAD
             // 
             this.bzero.Location = new System.Drawing.Point(12, 232);
             this.bzero.Name = "bzero";
-            this.bzero.Size = new System.Drawing.Size(50, 50);
+            this.bzero.Size = new System.Drawing.Size(106, 50);
             this.bzero.TabIndex = 4;
             this.bzero.Text = "0";
             this.bzero.UseVisualStyleBackColor = true;
             // 
+            // beight
+            // 
+            this.beight.Location = new System.Drawing.Point(68, 65);
+            this.beight.Name = "beight";
+            this.beight.Size = new System.Drawing.Size(50, 50);
+            this.beight.TabIndex = 5;
+            this.beight.Text = "8";
+            this.beight.UseVisualStyleBackColor = true;
+            // 
+            // bnine
+            // 
+            this.bnine.Location = new System.Drawing.Point(124, 65);
+            this.bnine.Name = "bnine";
+            this.bnine.Size = new System.Drawing.Size(50, 50);
+            this.bnine.TabIndex = 6;
+            this.bnine.Text = "9";
+            this.bnine.UseVisualStyleBackColor = true;
+            // 
+            // bfive
+            // 
+            this.bfive.Location = new System.Drawing.Point(68, 121);
+            this.bfive.Name = "bfive";
+            this.bfive.Size = new System.Drawing.Size(50, 50);
+            this.bfive.TabIndex = 7;
+            this.bfive.Text = "5";
+            this.bfive.UseVisualStyleBackColor = true;
+            // 
+            // bsix
+            // 
+            this.bsix.Location = new System.Drawing.Point(124, 121);
+            this.bsix.Name = "bsix";
+            this.bsix.Size = new System.Drawing.Size(50, 50);
+            this.bsix.TabIndex = 8;
+            this.bsix.Text = "6";
+            this.bsix.UseVisualStyleBackColor = true;
+            // 
+            // btwo
+            // 
+            this.btwo.Location = new System.Drawing.Point(68, 176);
+            this.btwo.Name = "btwo";
+            this.btwo.Size = new System.Drawing.Size(50, 50);
+            this.btwo.TabIndex = 9;
+            this.btwo.Text = "2";
+            this.btwo.UseVisualStyleBackColor = true;
+            // 
+            // bthree
+            // 
+            this.bthree.Location = new System.Drawing.Point(124, 177);
+            this.bthree.Name = "bthree";
+            this.bthree.Size = new System.Drawing.Size(50, 50);
+            this.bthree.TabIndex = 10;
+            this.bthree.Text = "3";
+            this.bthree.UseVisualStyleBackColor = true;
+            // 
+            // bdot
+            // 
+            this.bdot.Location = new System.Drawing.Point(124, 232);
+            this.bdot.Name = "bdot";
+            this.bdot.Size = new System.Drawing.Size(50, 50);
+            this.bdot.TabIndex = 11;
+            this.bdot.Text = ".";
+            this.bdot.UseVisualStyleBackColor = true;
+            // 
+            // baddition
+            // 
+            this.baddition.Location = new System.Drawing.Point(180, 232);
+            this.baddition.Name = "baddition";
+            this.baddition.Size = new System.Drawing.Size(50, 50);
+            this.baddition.TabIndex = 12;
+            this.baddition.Text = "+";
+            this.baddition.UseVisualStyleBackColor = true;
+            // 
+            // bsubtraction
+            // 
+            this.bsubtraction.Location = new System.Drawing.Point(180, 176);
+            this.bsubtraction.Name = "bsubtraction";
+            this.bsubtraction.Size = new System.Drawing.Size(50, 50);
+            this.bsubtraction.TabIndex = 13;
+            this.bsubtraction.Text = "-";
+            this.bsubtraction.UseVisualStyleBackColor = true;
+            // 
+            // bequal
+            // 
+            this.bequal.Location = new System.Drawing.Point(236, 177);
+            this.bequal.Name = "bequal";
+            this.bequal.Size = new System.Drawing.Size(50, 104);
+            this.bequal.TabIndex = 14;
+            this.bequal.Text = "=";
+            this.bequal.UseVisualStyleBackColor = true;
+            // 
+            // bmultiplication
+            // 
+            this.bmultiplication.Location = new System.Drawing.Point(180, 121);
+            this.bmultiplication.Name = "bmultiplication";
+            this.bmultiplication.Size = new System.Drawing.Size(50, 50);
+            this.bmultiplication.TabIndex = 15;
+            this.bmultiplication.Text = "*";
+            this.bmultiplication.UseVisualStyleBackColor = true;
+            // 
+            // bdivision
+            // 
+            this.bdivision.Location = new System.Drawing.Point(180, 65);
+            this.bdivision.Name = "bdivision";
+            this.bdivision.Size = new System.Drawing.Size(50, 50);
+            this.bdivision.TabIndex = 16;
+            this.bdivision.Text = "/";
+            this.bdivision.UseVisualStyleBackColor = true;
+            // 
+            // bclearentry
+            // 
+            this.bclearentry.Location = new System.Drawing.Point(236, 65);
+            this.bclearentry.Name = "bclearentry";
+            this.bclearentry.Size = new System.Drawing.Size(50, 50);
+            this.bclearentry.TabIndex = 17;
+            this.bclearentry.Text = "CE";
+            this.bclearentry.UseVisualStyleBackColor = true;
+            // 
+            // bclear
+            // 
+            this.bclear.Location = new System.Drawing.Point(236, 120);
+            this.bclear.Name = "bclear";
+            this.bclear.Size = new System.Drawing.Size(50, 50);
+            this.bclear.TabIndex = 18;
+            this.bclear.Text = "C";
+            this.bclear.UseVisualStyleBackColor = true;
+            // 
             // Body
             // 
-            this.ClientSize = new System.Drawing.Size(282, 325);
+            this.ClientSize = new System.Drawing.Size(296, 293);
+            this.Controls.Add(this.bclear);
+            this.Controls.Add(this.bclearentry);
+            this.Controls.Add(this.bdivision);
+            this.Controls.Add(this.bmultiplication);
+            this.Controls.Add(this.bequal);
+            this.Controls.Add(this.bsubtraction);
+            this.Controls.Add(this.baddition);
+            this.Controls.Add(this.bdot);
+            this.Controls.Add(this.bthree);
+            this.Controls.Add(this.btwo);
+            this.Controls.Add(this.bsix);
+            this.Controls.Add(this.bfive);
+            this.Controls.Add(this.bnine);
+            this.Controls.Add(this.beight);
             this.Controls.Add(this.bzero);
             this.Controls.Add(this.bone);
             this.Controls.Add(this.bfour);
@@ -105,6 +259,20 @@ namespace Calculator_BONDAD
         private System.Windows.Forms.Button bfour;
         private System.Windows.Forms.Button bone;
         private System.Windows.Forms.Button bzero;
+        private System.Windows.Forms.Button beight;
+        private System.Windows.Forms.Button bnine;
+        private System.Windows.Forms.Button bfive;
+        private System.Windows.Forms.Button bsix;
+        private System.Windows.Forms.Button btwo;
+        private System.Windows.Forms.Button bthree;
+        private System.Windows.Forms.Button bdot;
+        private System.Windows.Forms.Button baddition;
+        private System.Windows.Forms.Button bsubtraction;
+        private System.Windows.Forms.Button bequal;
+        private System.Windows.Forms.Button bmultiplication;
+        private System.Windows.Forms.Button bdivision;
+        private System.Windows.Forms.Button bclearentry;
+        private System.Windows.Forms.Button bclear;
     }
 }
 #endregion
